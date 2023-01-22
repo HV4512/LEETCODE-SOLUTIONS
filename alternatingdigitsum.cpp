4f596013ac15c03e4ba5f48dc1f05fc435deb39f
Harsh Vardhan <jobbykarn@gmail.com>
2023-01-22 10:43:42 +0530

    2544. Alternating Digit Sum

---
+++ alternatingdigitsum.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int alternateDigitSum(int n) {
+        int N, s = 0, c = 0;
+	int i = 1;
+	if (n == 10 || n == 100)
+	{
+		return 1;
+	}
+	N = n;
+	while (N != 0)
+	{
+		N /= 10;
+		c++;
+	}
+	while (n != 0)
+	{
+
+		int rem = n % 10;
+		if (c % 2 != 0)
+		{
+			if (i % 2 == 0)
+			{
+				s = s - rem;
+			}
+			else
+			{
+
+				s += rem;
+			}
+		}
+		else
+		{
+			if (i % 2 == 0)
+			{
+				s = s + rem;
+			}
+			else
+			{
+
+				s -= rem;
+			}
+		}
+		n /= 10;
+		i++;
+	}
+        return s;
+    
+        
+    }
+};
